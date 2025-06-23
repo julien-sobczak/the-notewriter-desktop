cd99dba4631300411481d2aa8d3477c29320d33f
Julien Sobczak <julien.sobczak@gmail.com>
2025-06-23 20:44:37 +0200

    feat: display quote author using a <footer> tag

--- src/renderer/Markdown.tsx
+++ src/renderer/Markdown.tsx
@@ -4,11 +4,25 @@ type MarkdownProps = {
   md: string;
 };
 
-// Custom renderer to add target="_blank" to all links
+// Custom renderer to customize the generated HTML
 const renderer = new marked.Renderer();
+// Add target="_blank" to all links
 renderer.link = ({ href, title, text }: Tokens.Link): string => {
   const titleAttr = title ? ` title="${title}"` : '';
   return `<a href="${href}"${titleAttr} target="_blank" rel="noopener noreferrer">${text}</a>`;
+};
+// Emphasize quotes author
+renderer.blockquote = ({ text }: Tokens.Blockquote): string => {
+  let result = '<blockquote class="markdown-quote">\n';
+  text.split('\n').forEach((line) => {
+    if (line.startsWith('— ') || line.startsWith('-- ')) {
+      const cite = line.slice(2).trim();
+      result += `<footer>— <cite>${cite}</cite></footer>\n`;
+    } else if (line.trim() !== '') {
+      result += `<p>${line}</p>\n`;
+    }
+  });
+  return result;
 };
 
 export default function Markdown({ md }: MarkdownProps) {
